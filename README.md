794b3c2a6d39e9d44b2a1abea5b1ab9f1c95982f
Taylor <taylorwhite21@yahoo.com>
2022-02-05 19:09:08 -0500

    completed lab

---
+++ README.md
@@ -0,0 +1,5 @@
+# My first web scraper
+This is my first web scraper tested for Wiki pages that will grab every mention of 'citation needed' and return the number of times it is mentioned and the passage it is mentioned in.
+
+No PR to apply at this time.
+
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Grabs the number of citations needed prompts
 def get_citations_needed_count(url):
   citations = 0
+  # grabs page information
   page = requests.get(url)
+  # Parses the information
   soup = BeautifulSoup(page.content, 'html.parser')
+  # Finds all <a> HTML tags.
   titles = soup.find_all('a')
+  # Loops through the <a> tags and finds the citation needed text and increments by 1
   for title in titles:
     if title.text == 'citation needed':
       citations+=1
@@ -14,59 +19,16 @@ def get_citations_needed_count(url):
 
 def get_citations_needed_report(url):
   passages = ''
+  # Next 3 lines are same as needed_count function
   page = requests.get(url)
   soup = BeautifulSoup(page.content, 'html.parser')
   titles = soup.find_all('p')
+  # Loops through found tags and appends the text if the paragraph has citation needed in it
   for title in titles:
     if 'citation needed' in title.text:
       passages += title.text
   passages = passages.replace("'\n '", ' ')
-  print(f'passages: {passages}')
   return passages
-   
-# def get_citations_needed_report(url):
-#   passages = ''
-#   page = requests.get(url)
-#   soup = BeautifulSoup(page.content, 'html.parser')
-#   titles = soup.find_all('p')
-#   for title in titles:
-#     if 'citation needed' in title.text:
-#       passages = title.text
-#   print(f'passages: {passages}')
-#   return passages
-  
 
 get_citations_needed_report('https://en.wikipedia.org/wiki/Search_for_extraterrestrial_intelligence')
 get_citations_needed_count('https://en.wikipedia.org/wiki/Search_for_extraterrestrial_intelligence')
-
-
-
-
-
-
-
-
-# anchors = results('a')
-# # print(anchors)
-# links = [anchor['href'] for anchor in anchors]
-# # print(links)
-
-# python_link = links[1]
-# # print(python_link)
-
-# new_url = 'https://testing-www.codefellows.org' + python_link
-# # print(new_url)
-
-# link_content = requests.get(new_url)
-# # print(link_content.content)
-# link_soup = BeautifulSoup(link_content.content, 'html.parser')
-# # print(link_soup)
-
-# article = link_soup('article')[1]
-# # print(article)
-
-# list_items = article.select(' ul li ul li ')
-# # print(list_items)
-
-# for li in list_items:
-#   print(li.text)
